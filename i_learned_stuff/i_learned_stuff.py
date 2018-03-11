0505a96fda16af6bdde6386ef0b8c63514594044
dajwarren <r01daw13@abdn.ac.uk>
2018-03-11 19:26:49 +0000

    Basic class structure and writing

--- i_learned_stuff/i_learned_stuff.py
+++ i_learned_stuff/i_learned_stuff.py
@@ -1,1 +1,13 @@
 import jsonpickle
+
+class LearnObject:
+
+    def __init__(self, logtime, message, tags):
+        self.log_time = logtime.strftime("%Y-%m-%d %H:%M:%S")
+        self.message = message
+        self.tags = list(tags)
+
+    def write_learnobject(self, logtime):
+        frozen = jsonpickle.encode(self)
+        with open("output/" + logtime.strftime("%Y-%m-%d_%H:%M:%S") + "_" + "learnlog.txt", "w") as outfile:
+            outfile.write(frozen)
