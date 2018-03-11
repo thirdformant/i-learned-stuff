ed99f11bb9290b9640784fc27bc68ae83ade39ef
dajwarren <r01daw13@abdn.ac.uk>
2018-03-11 19:28:21 +0000

    Creates learning log object and writes to json

--- i_learned_stuff/cli.py
+++ i_learned_stuff/cli.py
@@ -1,11 +1,19 @@
 import click
+import datetime
+
+from .i_learned_stuff import LearnObject
 
 @click.command()
 @click.argument("message")
 @click.option("--tags", "-t", multiple=True, help="Optional tags")
 
 def main(message, tags):
-    # click.echo("Message: {0}\nTags: {1}".format(message, tags))
+    click.echo("Message: {0}\nTags: {1}".format(message, tags))
+    # Create learn object from cli parameters
+    logtime = datetime.datetime.now()
+    learn_object = LearnObject(logtime, message, tags)
+    learn_object.write_learnobject(logtime)
+
 
 if __name__ == "__main__":
     main()
